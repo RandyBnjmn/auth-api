7a36c229e1c3f87fa447c6b21716d5b471de3b8c
randy.benjamin <randy.benjamin@economia.gob.do>
2023-12-01 21:22:18 -0400

    (feat): Login and Jwt done

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,19 @@ import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
     AuthModule,
     MongooseModule.forRoot(process.env.MONGO_URI),
+
+    JwtModule.register({
+      global: true,
+      secret: process.env.JWT_SECRET,
+      signOptions: { expiresIn: '1h' }
+    })
   ],
   controllers: [],
   providers: [],
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,17 +1,23 @@
-import { BadRequestException, Injectable, InternalServerErrorException } from '@nestjs/common';
+import { BadRequestException, Injectable, InternalServerErrorException, UnauthorizedException } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateAuthDto } from './dto/update-auth.dto';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { User } from './entities/user.entity';
 import * as bcrypt from "bcryptjs"
+import { LoginDto } from './dto/login.dto';
+import { JwtService } from '@nestjs/jwt';
+import { JwtPayload } from './interfaces/jwt-payload.interface';
 
 @Injectable()
 export class AuthService {
 
+
   constructor
     (
-      @InjectModel(User.name) private userModel: Model<User>
+      @InjectModel(User.name)
+      private userModel: Model<User>,
+      private jwtService: JwtService
     ) { }
 
 
@@ -41,6 +47,37 @@ export class AuthService {
     }
 
   }
+
+  async login(loginDto: LoginDto) {
+
+    const { email, password } = loginDto;
+
+    const user = await this.userModel.findOne({ email: email });
+    if (!user) {
+      throw new UnauthorizedException('Email or Password incorrect')
+    }
+    if (!bcrypt.compareSync(password, user.password)) {
+      throw new UnauthorizedException('Email or Password incorrect')
+    }
+
+    const { password: _, ...rest } = user.toJSON()
+
+
+    return {
+      ...rest,
+      token: this.generateJwt({ id: user.id })
+    }
+
+  }
+
+  //GenerateToken
+  private generateJwt(payload: JwtPayload): string {
+    const token = this.jwtService.sign(payload)
+
+    return token;
+
+  }
+
 
   findAll() {
     return `This action returns all auth`;
